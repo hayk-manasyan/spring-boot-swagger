5fa80c73dc47e46a1de972a70616007c28908602
Hayk Manasyan <hayk.manasyan@plugsurfing.com>
2020-04-26 17:16:14 +0400

    - Spring boot application with integrated Swagger

---
+++ src/main/java/com/project/io/demo/resources/HelloController.java
@@ -0,0 +1,17 @@
+package com.project.io.demo.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public ResponseEntity<?> hello()
+    {
+        return ResponseEntity.ok().body("Hello Spring.");
+    }
+}
